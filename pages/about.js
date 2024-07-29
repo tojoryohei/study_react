a46a7615b9ab7edcc30c0bcf9d9a505dae04e367
tojoryohei <tojoryohei@yahoo.co.jp>
2024-07-29 20:47:26 +0900

    props

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Header } from "../conponents/Header"
+import { Headline } from "../conponents/Headline"
 import { Links } from "../conponents/Links"
 import styles from "@/styles/Home.module.css";
 
@@ -12,40 +13,7 @@ export default function Home() {
     <>
       <Header/>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/about.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
+        <Headline title="about"/>
         <Links/>
       </main>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { Inter } from "next/font/google";
 import { Header } from "../conponents/Header"
+import { Headline } from "../conponents/Headline"
 import { Links } from "../conponents/Links"
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,41 +10,7 @@ export default function Home() {
     <>
       <Header/>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Thanks by{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
+        <Headline title="index"/>
         <Links/>
       </main>
     </>
