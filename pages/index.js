a2f42ec23260b7bf6555afcadfacedcc793e7c00
tojoryohei <tojoryohei@yahoo.co.jp>
2024-07-29 12:41:35 +0900

    Thanks

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Powered by{" "}
+              Thanks by{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
